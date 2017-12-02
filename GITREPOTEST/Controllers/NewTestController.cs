37c47cb253ce860221e9664ad7e553a1887a0a65
pavan2017git <pavan.tmca@hotmail.com>
2017-12-02 15:01:51 +0530

    NewTest controller added

---
+++ GITREPOTEST/Controllers/NewTestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GITREPOTEST.Controllers
+{
+    public class NewTestController : Controller
+    {
+        //
+        // GET: /NewTest/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
